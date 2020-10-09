2eb221f33c5c37f6ce99529b2e8706ec1ff02b55
ivan.zrobok <ivan.zrobok.itis.2020@lpnu.ua>
2020-10-09 17:21:52 +0300

    first master commit

---
+++ ConsoleApplication_lab2.2/ConsoleApplication_lab2.2.cpp
@@ -0,0 +1,30 @@
+﻿// ConsoleApplication_lab2.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// Lab_02.cpp
+// < Зробок Іван>
+// Лабораторна робота № 2.1
+// Лінійні програми.
+// Варіант 0.9
+//Z1=(cos α-cos β)2-(sin α-sin β)2;
+//Z2=-4sin2(α-β)/2 * cos(α+β).
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a;//вхідний параметр
+    double b;//вхідний параметр
+    double z1; //результат обчислення 1 - го виразу
+    double z2;// результат обчислення 1-го виразу
+
+    cout << "a= "; cin >> a;
+    cout << "b= "; cin >> b;
+    z1 = (cos(a) - cos(b)) * (cos(a) - cos(b)) - ((sin(a) - sin(b)) * (sin(a) - sin(b)));
+    z2 = -4 * sin((a - b) / 2) * sin((a - b) / 2) * cos(a + b);
+
+    cout << endl;
+    cout << "z1 =" << z1 << endl;
+    cout << "z2 =" << z2 << endl;
+
+    cin.get();
+    return 0;
+
+}
